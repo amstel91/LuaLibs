3e51d822212ff1a109db54704cdb83c9c7a0effe
Amstel <amstel91@gmail.com>
2019-03-13 23:40:42 +0530

    Adding more string methods

--- test/strlTest.lua
+++ test/strlTest.lua
@@ -13,3 +13,24 @@ assert(strl.trim("  Hello")=="Hello","trim with left padding")
 assert(strl.trim("  Hello  ")=="Hello","trim with both padding")
 assert(strl.trim("Hello")=="Hello","trim with no padding")
 
+assert(strl.ltrim("  Hello  ")=="Hello  ","ltrim")
+assert(strl.rtrim("  Hello  ")=="  Hello","rtrim")
+
+assert(#strl.split("Hello World")==2,"split by space")
+assert(#strl.split(" Hello World ")==4,"split by space")
+assert(#strl.split("Hello-World","-")==2,"split by hyphen")
+assert(#strl.split("Hello, New, World",",")==3,"split by comma")
+
+assert(#strl.splitLines("Hello\n World")==2,"split by line \n")
+assert(#strl.splitLines("Hello\r\n World")==2,"split by line \r\n")
+assert(#strl.splitLines("Hello\r World")==2,"split by line \r")
+
+assert(strl.startsWith("HelloWorld","Hello"),"startsWith Hello")
+assert(strl.startsWith("HelloWorld","World")==false,"startsWith World")
+
+assert(strl.endsWith("HelloWorld","World"),"endsWith World")
+assert(strl.endsWith("HelloWorld","Hello")==false,"endsWith Hello")
+
+assert(strl.encodeBase64("test:test123")=="dGVzdDp0ZXN0MTIz","Encode base64")
+assert(strl.decodeBase64("dGVzdDp0ZXN0MTIz")=="test:test123","Decode base64")
+

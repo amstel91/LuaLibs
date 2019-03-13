9d45696f285e109c15d412b27d7c971d4f94f9b0
Amstel D'Almeida <37899126+amstel91@users.noreply.github.com>
2019-03-13 10:49:52 +0530

    Addinf isString,trim,isEmpty

--- strl.lua
+++ strl.lua
@@ -1,1 +1,15 @@
 local strl={}
+
+function strl.isString(s)
+  return type(s)=="string"
+end
+
+function strl.isEmpty(s)
+  return s == nil or s == ''
+end
+
+function strl.trim(s)
+  return string.gsub(s, "^%s*(.-)%s*$", "%1")
+end
+
+return strl
